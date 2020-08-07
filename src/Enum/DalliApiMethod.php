02331560fcaf9ff680d2c7dddd99e76ddec9a879
floor12 <floor12@floor12.net>
2020-08-07 14:34:50 +0200

    Api method list added.

---
+++ src/Enum/DalliApiMethod.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace floor12\DalliApi\Enum;
+
+
+class DalliApiMethod
+{
+    const BASKET_CREATE = 'basketcreate';
+    const BASKET_UPDATE = 'editbasket';
+    const BASKET_GET = 'getbasket';
+    const BASKET_CLEAR = 'removebasket';
+    const BASKET_CONFIRM = 'sendbasket';
+    const ACT = 'getact';
+    const STICKERS = 'stickers';
+    const ORDER_STATUS = 'statusreq';
+    const SERVICES_LIST = 'services';
+    const PVZ_LIST = 'pvzlist';
+    const DELIVERY_COST = 'deliverycost';
+}
